dbe542251a53f356785fa42796c52e246644427e
Sebastian Echazu <sebastianechazu@outlook.com>
2024-07-04 10:28:40 -0300

    ajuste en importaciones

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
+// Angular
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,ReactiveFormsModule } from '@angular/forms';
+// PrimeNG
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
@@ -8,9 +10,10 @@ import { InputTextModule } from 'primeng/inputtext';
   selector: 'app-login',
   standalone: true,
   imports: [
-    CardModule, 
-    ButtonModule, 
-    InputTextModule],
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -18,9 +21,12 @@ export class LoginComponent {
 
   loginForm = new FormGroup({
     mail: new FormControl(''),
-    password: new FormControl(''),
+    password: new FormControl('')
 
   });
+
+
+
   onSubmit() {
     console.warn(this.loginForm.value);
   }
